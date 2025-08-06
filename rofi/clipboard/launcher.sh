5f251fee959cc2bf1c09af97efba7ed35d6bcca0
Ayush <ayushdumasia00@gmail.com>
2025-08-06 17:03:09 +0530

    fix 🐞: Rofi Clipboard

--- rofi/clipboard/launcher.sh
+++ rofi/clipboard/launcher.sh
@@ -7,22 +7,22 @@ fi
 
 dir="$HOME/.config/rofi/clipboard"
 
-choice=$(echo -e "\t\uf1f8   Wipe Clipboard\n$(cliphist list)" | rofi -markup-rows -dmenu -display-columns 2 -theme ${dir}/clipboard.rasi)
+choice=$(echo -e "\t\uf1f8   Wipe Clipboard\n$(cliphist list)" | rofi -markup-rows -dmenu  -display-columns 2 -theme ${dir}/clipboard.rasi)
 
 if [[ $choice == *"Wipe Clipboard"* ]]; then
-    yes=''
-    no=''
-
+    yes=''
+    no=''
+    
     confirmation=$(echo -e "<span foreground='#a6e3a1'>$yes</span>\n<span foreground='#f38ba8'>$no</span>" | \
-        rofi -markup-rows -dmenu -p 'Confirmation' -mesg 'Are you Sure?' -theme ${dir}/confirmation.rasi)
-
+    rofi -markup-rows -dmenu -p 'Confirmation' -mesg 'Are you Sure?' -theme ${dir}/confirmation.rasi)
+    
     if [[ $confirmation =~ "$yes" ]]; then
         cliphist wipe
         wl-copy -c
         dunstify -h string:x-dunst-stack-tag:clip_notif -t 4000 -u critical "Clipboard Manager" "Clipboard has been wiped"
     fi
     exit
-elif [[ -n $choice ]]; then
+    elif [[ -n $choice ]]; then
     cliphist decode "$choice" | wl-copy
     wtype -M ctrl -M shift -P v -s 500 -p v -m shift -m ctrl
 else
