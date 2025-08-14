b35d2185f6de433099fab75a9be2ea36ef176f35
Ayush <ayushdumasia00@gmail.com>
2025-08-14 23:49:30 +0530

    refactor 🔨: Misc changes

--- rofi/wallselect/script.sh
+++ rofi/wallselect/script.sh
@@ -3,9 +3,20 @@
 
 WALL_DIR="$HOME/Pictures/Wallpaper"
 
-SELECTED=$(find ~/Pictures/Wallpaper -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) \
-    | while read -r img; do echo -en  "$img\0icon\x1f$img\n" ; done \
-| rofi -dmenu -show-icons -theme "$HOME/.config/rofi/wallselect/style.rasi" )
+
+# SELECTED=$(find ~/Pictures/Wallpaper -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) \
+#     | while read -r img; do echo -en  "$img\0icon\x1f$img\n" ; done \
+# | rofi -dmenu -show-icons -theme "$HOME/.config/rofi/wallselect/style.rasi" )
+
+
+SELECTED=$(find ~/Pictures/Wallpaper \
+    \( -path ~/Pictures/Wallpaper/catppuccin -o -path ~/Pictures/Wallpaper/temp \) -prune -o \
+    -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \)  \
+    | shuf |
+    while read -r img; do
+        echo -en "$img\0icon\x1f$img\n"
+    done \
+| rofi -dmenu -show-icons -theme "$HOME/.config/rofi/wallselect/style.rasi")
 
 # echo $SELECTED
 
